58528e61b7eb4d851060ec5e4136fd25c1a9304b
Radhika-2002 <2019radhika.katiyara@ves.ac.in>
2024-05-13 13:32:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function fibonacci(num) {
-// your code here
+function fibonacci1(num) {
+  let arr = [0, 1];
+  for (let i = 2; i <= num; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+  return arr[num];
 }
 
 module.exports = fibonacci;
