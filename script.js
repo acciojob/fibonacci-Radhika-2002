0c71beb0267957c45afa57d3e55575034302ab31
Radhika-2002 <2019radhika.katiyara@ves.ac.in>
2024-06-02 13:55:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-function fibonacci(num) {
-    if (num === 0) return 0;
-    if (num === 1) return 1;
-
-    let a = 0, b = 1, fib;
-    for (let i = 2; i <= num; i++) {
-        fib = a + b;
-        a = b;
-        b = fib;
-    }
-    return fib;
+function fibonacci(num){
+	if(num == 0){
+		return 0;
+	}else if(num == 1){
+		return 1;
+	}
+	return fibonacci(num-1) + fibonacci(num-2);
 }
+module.exports = fibonacci;
